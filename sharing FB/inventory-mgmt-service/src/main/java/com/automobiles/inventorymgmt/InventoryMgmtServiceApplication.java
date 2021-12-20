b9f8f4e8717aabcda67959797eb4793960260e64
Rockycta <rocky.cta@gmail.com>
2021-12-20 12:47:04 +0530

    First time adding a project to git repo from Sts tool

---
+++ sharing FB/inventory-mgmt-service/src/main/java/com/automobiles/inventorymgmt/InventoryMgmtServiceApplication.java
@@ -0,0 +1,12 @@
+package com.automobiles.inventorymgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryMgmtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryMgmtServiceApplication.class, args);
+	}
+}
